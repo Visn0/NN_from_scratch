00234556f11d85a3e416bba80bc66370416bf553
carlosarismendiua <ceas1@alu.ua.es>
2021-01-24 16:43:54 +0100

    Arreglado preprocess

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -2,53 +2,33 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-# df = pd.read_csv('y.csv', sep=',', decimal='.')
-# pd.set_option('display.max_rows', None)
-# pd.set_option('display.max_columns', None)
-# pd.set_option('display.width', 2000)
-# pd.set_option('display.float_format', '{:20,.2f}'.format)
-# pd.set_option('display.max_colwidth', None)
-# print(df.describe(include='all').T)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 2000)
+pd.set_option('display.float_format', '{:20,.2f}'.format)
+pd.set_option('display.max_colwidth', None)
 
 # RAM min percentage of variation (not included)
 MIN_PERCENTAGE = -10.0
 
 def balance_dataset(X, y):    
     x_df = pd.DataFrame(data=X)
-    y_df = pd.DataFrame(data=y, columns=list('abcde'))
-
-    result = pd.concat([x_df, y_df], axis=1)
+    y_df = pd.DataFrame(data=y, columns=list('abcde'))    
+    result = pd.concat([x_df, y_df], axis=1)    
 
     nomove  = result[(result.a == 0) & (result.b == 0) & (result.c == 0) & (result.d == 0) & (result.e == 0)].copy()
     move    = result[(result.a == 1) | (result.b == 1) | (result.c == 1) | (result.d == 1) | (result.e == 1)].copy()
 
     nomove = nomove[:int(nomove.shape[0]/4)].copy()
-
-    result = pd.concat([nomove, move])
-
-    pd.set_option('display.max_rows', None)
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.width', 2000)
-    pd.set_option('display.float_format', '{:20,.2f}'.format)
-    pd.set_option('display.max_colwidth', None)
-    # print(result.describe(include='all').T)
+    result = pd.concat([nomove, move])    
     
-    countClicksPd(result)    
+    countClicks(result)    
 
-    print(result)
     X_res = result.iloc[:, :-5]    
     y_res = result.iloc[:, -5:]
 
-    return X_res, y_res
+    return X_res, y_res    
 
-
-
-def printArrayAsTable(rows, cols, array):
-    index = 0
-    for i in range(0, rows, 1):
-        for j in range(0, cols, 1):
-            print(f'{array[index]}', sep=',')
-            index += 1
 
 def generateRamIndexes(X, minPercentage):
     heatMap = np.zeros(X[0].shape[0])
@@ -92,31 +72,15 @@ def parseOutputTo1KeyAction(y):
         
         y_res.append( np.array([up, spaceLeft, spaceRight, left, right]) )
 
-    y_df = pd.DataFrame(data=y_res)
-    return y_df
+    y_res = np.asarray(y_res)
+    return y_res
 
-def countClicksPd(y):
+def countClicks(y):
     up          = y[(y.a == 1)].shape[0]
     spaceLeft   = y[(y.b == 1)].shape[0]
     spaceRight  = y[(y.c == 1)].shape[0]
     left        = y[(y.d == 1)].shape[0]
     right       = y[(y.e == 1)].shape[0]
-
-    print('(UP, SPACE+LEFT, SPACE+RIGHT, LEFT, RIGHT)') 
-    print(f'({up}, {spaceLeft}, {spaceRight}, {left}, {right})') 
-
-
-    up          = 0
-    spaceLeft   = 0
-    spaceRight  = 0
-    left        = 0
-    right       = 0
-    for i in range(y.shape[0]):
-        up          += (1 if y[i, 0] == 1 else 0)
-        spaceLeft   += (1 if y[i, 1] == 1 else 0)
-        spaceRight  += (1 if y[i, 2] == 1 else 0)
-        left        += (1 if y[i, 3] == 1 else 0)
-        right       += (1 if y[i, 4] == 1 else 0)
 
     print('(UP, SPACE+LEFT, SPACE+RIGHT, LEFT, RIGHT)') 
     print(f'({up}, {spaceLeft}, {spaceRight}, {left}, {right})') 
@@ -129,29 +93,32 @@ def removeUnusedIndexes(X, indexes):
 
     # Iterates through the dataset examples    
     for i in range(0, X.shape[0], 1):
-        x = np.take(X[i], indexes)          
+        x = np.take(X[i], indexes)
         X_res.append(x)        
     
     X_res = np.asarray(X_res)
     return X_res
 
 def save_dataset(X, y, label):
-    # np.savetxt(f"X_{label}.csv", X, delimiter=",", fmt='%i')
-    # np.savetxt(f"y_{label}.csv", y, delimiter=",", fmt='%i')
     X.to_csv(f'X_{label}.csv', index=False, header=False)
     y.to_csv(f'y_{label}.csv', index=False, header=False)
 
+def print_results(X_train, y_train, X_test, y_test, indexes):
+    print(f'X_train: {X_train.shape}, y_train: {y_train.shape}')
+    print(f'X_test: {X_test.shape}, y_test: {y_test.shape}')
+    print(f'Indexes shape: {indexes.shape}')
+    print(f'Used indexes: {indexes}')
+
 def main():        
     # Load the whole dataset X, y
-    X = pd.read_csv('x.csv', sep=',', dtype=int)
-
-    y = np.read_csv('y.csv', sep=',', dtype=int)
+    X = np.genfromtxt('x.csv', delimiter=',', dtype=int)
+    y = np.genfromtxt('y.csv', delimiter=',', dtype=int)
     
     # Generate the indexes that must be taken into account when training the Net
     indexes = generateRamIndexes(X, MIN_PERCENTAGE)
 
     # Remove unused indexes from dataset (doesn't modify the original files)
-    # X = removeUnusedIndexes(X, indexes)
+    X = removeUnusedIndexes(X, indexes)
     y = parseOutputTo1KeyAction(y)
 
     X, y = balance_dataset(X, y)
@@ -162,5 +129,7 @@ def main():
     # Save dataset
     save_dataset(X_train, y_train, 'train')
     save_dataset(X_test, y_test, 'test')
+
+    print_results(X_train, y_train, X_test, y_test, indexes)
 
 main()

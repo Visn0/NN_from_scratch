9f42172b1afb342c640c628542a18d2af91406bc
carlosarismendiua <ceas1@alu.ua.es>
2021-01-26 20:47:54 +0100

    Bot entregable

--- main_ale.cpp
+++ main_ale.cpp
@@ -3,8 +3,8 @@
 #include <string.h>
 
 const double REGULARIZATION_LAMBDA  = 0.001;
-const uint32_t BATCH_SIZE           = 128;
-const int EPOCHS                    = 100;
+const uint32_t BATCH_SIZE           = 96;
+const int EPOCHS                    = 50;
 const double LEARNING_RATE          = 0.1;
 const int OUTPUT_SIZE               = 3;
 uint16_t INPUT_SIZE                 = 0;
@@ -59,7 +59,7 @@ void run(int argc, char* argv[])
     INPUT_SIZE = X_train[0].size();
     std::cout << "INPUT_SIZE: " << INPUT_SIZE << std::endl;
     std::cout << "X_train SIZE: " << X_train.size() << std::endl;
-    Net_t net{ INPUT_SIZE, 32, 32, OUTPUT_SIZE };
+    Net_t net{ INPUT_SIZE, 28, 22, OUTPUT_SIZE };
     
     const double fit_time = net_fit(net, X_train, y_train, X_valid, y_valid);
 
